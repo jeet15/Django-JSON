b75f6a9e161279ba03f5e1e4e612e723883ef2df
Gurjeet Singh <gurjeetsingh1506@gmail.com>
2014-11-19 11:48:06 +0530

    assignment

--- cars/static/js/sample.js
+++ cars/static/js/sample.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-    var $vl = $("#back");
 
-    $vl.css("background", "#99CCFF");
+    $("#back").css("background", "#99CCFF");
 
     setTimeout(function () {
-        $vl.css("background", "yellow");
+        $("#back").css("background", "yellow");
     }, 2000);
 
 
     $("#btn1").click(function(){
         alert("This is the link"+ $("#ht1").attr("href"))
     });
+    
     $("#btn2").click(function(){
         alert("This is the value "+ $("#txt").val())
     });
